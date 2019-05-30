112581b55801aca38b59b0966dd90db50f6fbd10
Alex Moussa <alexmoussa@Alexs-MacBook-Pro.local>
2019-05-30 18:27:15 -0400

    added JQuery sessionStorage to hold the value of search bars

--- javascript/Standard.js
+++ javascript/Standard.js
@@ -1,40 +1,59 @@
-/* ---- storing cookies on which language is selected ---- */
-var language = sessionStorage.getItem('language');
-if ((language == 'EN') || (language == null)){
-  switchEnglish();
-}
-else{
-  switchFrench();
-}
+$(document).ready(function() {
 
-$('#english').click(function(){
-  switchEnglish();
-    sessionStorage.setItem('language', 'EN');
-});
-function switchEnglish() {
-  if ($('.EN').hasClass('active')){
-    $('.EN').toggleClass('active');
-    $('.FR').toggleClass('active');
-    $('#english').addClass('active');
-    $('#french').removeClass('active');
-  }
-else{
-  $('.EN').toggleClass('');
-  $('.FR').toggleClass('');
-} }
+  /* ---- storing cookies on which language is selected ---- */
+  var searchCuisine = sessionStorage.getItem('searchCuisine');
+  var searchCity = sessionStorage.getItem('searchCity');
+  var searchAddress = sessionStorage.getItem('searchAddress');
 
-$('#french').click(function(){
-  switchFrench();
-    sessionStorage.setItem('language', 'FR');
+  $('#searchCuisineId').val(searchCuisine);
+  $('#searchCityId').val(searchCity);
+  $('#searchAddressId').val(searchAddress);
+
+$('#searchButtonId').click(function(){
+    sessionStorage.setItem('searchCuisine', $('#searchCuisineId').val());
+    sessionStorage.setItem('searchCity', $('#searchCityId').val());
+    sessionStorage.setItem('searchAddress', $('#searchAddressId').val());
 });
-function switchFrench() {
-  if ($('.EN').hasClass('active')){
-    $('.EN').toggleClass('');
-    $('.FR').toggleClass('');
+
+// Global variable to store current value
+var oldValue = '';
+// Blank value when click to activate all options
+$('input').on('click', function() {
+  oldValue = $(this).val();
+  $(this).val('');
+});
+// Restore current value after click
+$('input').on('mouseleave', function() {
+  if ($(this).val() == '') {
+    $(this).val(oldValue);
   }
-else{
-  $('.EN').toggleClass('active');
-  $('.FR').toggleClass('active');
-  $('#english').removeClass('active');
-  $('#french').addClass('active');
-} }
+});
+
+// function switchEnglish() {
+//   if ($('.EN').hasClass('active')){
+//     $('.EN').toggleClass('active');
+//     $('.FR').toggleClass('active');
+//     $('#english').addClass('active');
+//     $('#french').removeClass('active');
+//   }
+// else{
+//   $('.EN').toggleClass('');
+//   $('.FR').toggleClass('');
+// } }
+//
+// $('#french').click(function(){
+//   switchFrench();
+//     sessionStorage.setItem('language', 'FR');
+// });
+// function switchFrench() {
+//   if ($('.EN').hasClass('active')){
+//     $('.EN').toggleClass('');
+//     $('.FR').toggleClass('');
+//   }
+// else{
+//   $('.EN').toggleClass('active');
+//   $('.FR').toggleClass('active');
+//   $('#english').removeClass('active');
+//   $('#french').addClass('active');
+// } }
+});
