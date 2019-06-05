26cbccc6506387198844933690543a1a78bc6a07
Alex Moussa <alexmoussa@Alexs-MacBook-Pro.local>
2019-06-05 11:27:04 -0400

    Jquery to add menu items to order page

--- javascript/Standard.js
+++ javascript/Standard.js
@@ -14,6 +14,7 @@ $('#searchButtonId').click(function(){
     sessionStorage.setItem('searchAddress', $('#searchAddressId').val());
 });
 
+
 // Global variable to store current value
 var oldValue = '';
 // Blank value when click to activate all options
@@ -61,39 +62,160 @@ $('#restaurantGreekId').click(function(){
 
 
 /* ---- storing cookies on selected menu items ---- */
-var foodNum;
-var $newItem = $( "<span id='orderItem'"+foodNum+" '></span>" ),
+var basketItemsNum = sessionStorage.getItem('basketItemsNum');
+console.log(basketItemsNum);
+if(basketItemsNum == null){
+  basketItemsNum = 0;
+}
+$('#basketNum').html(basketItemsNum);
 
-$( "#orderMenu" ).append( $newdiv1, [ newdiv2, existingdiv1 ] );
+var obj = {}
+var price = {}
 
+for(var i=1; i<7; i++){
+  obj["menuItemNum"+i] = sessionStorage.getItem('menuItemNum'+i);
+  obj["menuItem"+i] = sessionStorage.getItem('menuItem'+i);
+  price["priceItem"+i] = sessionStorage.getItem('priceItem'+i);
+  if(obj["menuItemNum"+i] == null){
+    obj["menuItemNum"+i] = 0;
+  }
+  console.log(obj);
+}
 
+$('#menuItemButt1').click(function(){
+    sessionStorage.setItem('menuItem1', $('#menuItem1').html());
+    sessionStorage.setItem('priceItem1', $('#priceItem1').html());
+    sessionStorage.setItem('menuItemNum1', ++obj["menuItemNum1"]);
+    sessionStorage.setItem('basketItemsNum', ++basketItemsNum);
+    $('#basketNum').html(basketItemsNum);
+});
+$('#menuItemButt2').click(function(){
+    sessionStorage.setItem('menuItem2', $('#menuItem2').html());
+    sessionStorage.setItem('priceItem2', $('#priceItem2').html());
+    sessionStorage.setItem('menuItemNum2', ++obj["menuItemNum2"]);
+    sessionStorage.setItem('basketItemsNum', ++basketItemsNum);
+    $('#basketNum').html(basketItemsNum);
+});
+$('#menuItemButt3').click(function(){
+    sessionStorage.setItem('menuItem3', $('#menuItem3').html());
+    sessionStorage.setItem('priceItem3', $('#priceItem3').html());
+    sessionStorage.setItem('menuItemNum3', ++obj["menuItemNum3"]);
+    sessionStorage.setItem('basketItemsNum', ++basketItemsNum);
+    $('#basketNum').html(basketItemsNum);
+});
+$('#menuItemButt4').click(function(){
+    sessionStorage.setItem('menuItem4', $('#menuItem4').html());
+    sessionStorage.setItem('priceItem4', $('#priceItem4').html());
+    sessionStorage.setItem('menuItemNum4', ++obj["menuItemNum4"]);
+    sessionStorage.setItem('basketItemsNum', ++basketItemsNum);
+    $('#basketNum').html(basketItemsNum);
+});
+$('#menuItemButt5').click(function(){
+    sessionStorage.setItem('menuItem5', $('#menuItem5').html());
+    sessionStorage.setItem('priceItem5', $('#priceItem5').html());
+    sessionStorage.setItem('menuItemNum5', ++obj["menuItemNum5"]);
+    sessionStorage.setItem('basketItemsNum', ++basketItemsNum);
+    $('#basketNum').html(basketItemsNum);
+});
+$('#menuItemButt6').click(function(){
+    sessionStorage.setItem('menuItem6', $('#menuItem6').html());
+    sessionStorage.setItem('priceItem6', $('#priceItem6').html());
+    sessionStorage.setItem('menuItemNum5', ++obj["menuItemNum6"]);
+    sessionStorage.setItem('basketItemsNum', ++basketItemsNum);
+    $('#basketNum').html(basketItemsNum);
+});
 
+/* ---- html code of selected menu items injected in order page ---- */
+var priceTotal=0;
+for(var i=1; i<7; i++){
+  if(obj["menuItemNum"+i] > 0){
+    $( "#orderRemove" ).append( "<span class='orderRemove' id='menuItemRemove"+i+"'><i class='fas fa-minus-circle' id='menuItem"+i+"'></i></span>" );
+    $( "#orderMenu" ).append( "<span class='orderItem' id='menuItemRemove"+i+"'>"+obj["menuItem"+i]+"("+obj["menuItemNum"+i]+")</span>" );
+    $( "#orderPrice" ).append( "<span class='orderItem' id='menuItemRemove"+i+"'>$"+price["priceItem"+i]+"</span>" );
+    priceTotal = priceTotal + (parseFloat(price["priceItem"+i]) * parseFloat(obj["menuItemNum"+i]));
+  }
+}
+$( "#orderRemove" ).append( "<span class='orderRemove'><i class='fas fa-receipt'></i></span>" );
+$( "#orderMenu" ).append( "<span class='orderItem'>Total: </span>" );
+$( "#orderPrice" ).append( "<span class='orderItem'>$"+priceTotal.toFixed(2)+"</span>" );
 
-// function switchEnglish() {
-//   if ($('.EN').hasClass('active')){
-//     $('.EN').toggleClass('active');
-//     $('.FR').toggleClass('active');
-//     $('#english').addClass('active');
-//     $('#french').removeClass('active');
-//   }
-// else{
-//   $('.EN').toggleClass('');
-//   $('.FR').toggleClass('');
-// } }
-//
-// $('#french').click(function(){
-//   switchFrench();
-//     sessionStorage.setItem('language', 'FR');
-// });
-// function switchFrench() {
-//   if ($('.EN').hasClass('active')){
-//     $('.EN').toggleClass('');
-//     $('.FR').toggleClass('');
-//   }
-// else{
-//   $('.EN').toggleClass('active');
-//   $('.FR').toggleClass('active');
-//   $('#english').removeClass('active');
-//   $('#french').addClass('active');
-// } }
+/* ---- removing menu items ---- */
+$( "#menuItem1" ).click(function(){
+  if(obj["menuItemNum1"] < 1){
+  }
+    sessionStorage.setItem('menuItemNum1', --obj["menuItemNum1"]);
+    priceTotal = priceTotal - (parseFloat(price["priceItem1"]) * parseFloat(obj["menuItemNum1"]));
+    sessionStorage.setItem('basketItemsNum', --basketItemsNum);
+    $('#basketNum').html(basketItemsNum);
+    if(obj["menuItemNum1"] == 1){
+      $("span").remove("#menuItemRemove1");
+    }
+      location.reload();
+});
+
+$( "#menuItem2" ).click(function(){
+  if(obj["menuItemNum2"] < 1){
+  }
+    sessionStorage.setItem('menuItemNum2', --obj["menuItemNum2"]);
+    priceTotal = priceTotal - (parseFloat(price["priceItem2"]) * parseFloat(obj["menuItemNum2"]));
+    sessionStorage.setItem('basketItemsNum', --basketItemsNum);
+    $('#basketNum').html(basketItemsNum);
+    if(obj["menuItemNum2"] == 1){
+      $("span").remove("#menuItemRemove2");
+    }
+      location.reload();
+});
+
+$( "#menuItem3" ).click(function(){
+  if(obj["menuItemNum3"] < 1){
+  }
+    sessionStorage.setItem('menuItemNum3', --obj["menuItemNum3"]);
+    priceTotal = priceTotal - (parseFloat(price["priceItem3"]) * parseFloat(obj["menuItemNum3"]));
+    sessionStorage.setItem('basketItemsNum', --basketItemsNum);
+    $('#basketNum').html(basketItemsNum);
+    if(obj["menuItemNum3"] == 1){
+      $("span").remove("#menuItemRemove3");
+    }
+      location.reload();
+});
+
+$( "#menuItem4" ).click(function(){
+  if(obj["menuItemNum4"] < 1){
+  }
+    sessionStorage.setItem('menuItemNum4', --obj["menuItemNum4"]);
+    priceTotal = priceTotal - (parseFloat(price["priceItem4"]) * parseFloat(obj["menuItemNum4"]));
+    sessionStorage.setItem('basketItemsNum', --basketItemsNum);
+    $('#basketNum').html(basketItemsNum);
+    if(obj["menuItemNum4"] == 1){
+      $("span").remove("#menuItemRemove4");
+    }
+      location.reload();
+});
+
+$( "#menuItem5" ).click(function(){
+  if(obj["menuItemNum5"] < 1){
+  }
+    sessionStorage.setItem('menuItemNum5', --obj["menuItemNum5"]);
+    priceTotal = priceTotal - (parseFloat(price["priceItem5"]) * parseFloat(obj["menuItemNum5"]));
+    sessionStorage.setItem('basketItemsNum', --basketItemsNum);
+    $('#basketNum').html(basketItemsNum);
+    if(obj["menuItemNum5"] == 1){
+      $("span").remove("#menuItemRemove5");
+    }
+      location.reload();
+});
+
+$( "#menuItem6" ).click(function(){
+  if(obj["menuItemNum6"] < 1){
+  }
+    sessionStorage.setItem('menuItemNum6', --obj["menuItemNum6"]);
+    priceTotal = priceTotal - (parseFloat(price["priceItem6"]) * parseFloat(obj["menuItemNum6"]));
+    sessionStorage.setItem('basketItemsNum', --basketItemsNum);
+    $('#basketNum').html(basketItemsNum);
+    if(obj["menuItemNum6"] == 1){
+      $("span").remove("#menuItemRemove6");
+    }
+      location.reload();
+});
+
 });
